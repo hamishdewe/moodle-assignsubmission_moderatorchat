51e5d8477c869d06ce3df267e526afff90c6d809
Hamish Dewe <hamish.dewe@gmail.com>
2020-02-27 09:49:43 +1300

    first commit

---
+++ db/events.php
@@ -0,0 +1,9 @@
+<?php
+
+$observers = array(
+
+    // array(
+    //     'eventname'   => '\mod_assign\event\marker_updated',
+    //     'callback'    => 'assign_submission_moderatorchat::observe_marker_updated',
+    // ),
+);
